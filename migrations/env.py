16e8b4dee89fbfad43871aa9b9b90a047cf56bd6
Debojyoti Chattoraj <debojyotichattoraj1996@gmail.com>
2025-07-14 21:44:01 +0530

    troubleshooting database migration

--- migrations/env.py
+++ migrations/env.py
@@ -57,9 +57,9 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    section = config.get_section(config.config_ini_section) or {}
+    
     connectable = engine_from_config(
-        section = section,
+        config.get_section(config.config_ini_section) or {},
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
